29f3fa1d44b915bb5b7648addc0707e54d22934c
159965 UlisesHernandez <44282854+UlisesRH@users.noreply.github.com>
2019-02-05 12:44:14 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio1_helloworld.java
@@ -0,0 +1,10 @@
+/*Ulises Hernandez 05/02/2019 12:18*/
+
+public class ejercicio1_helloworld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		System.out.println("Ulises Hernandez");
+	}
+}
