b08e9b5d0344931087f22f556bbd88db4a3bff3e
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-07 12:55:54 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio10_formula.java
@@ -0,0 +1,14 @@
+/*Nombre:Ulises Hernandez */
+/*Dia: 07/02/2019 */
+/*Hora: 12:49*/
+
+
+
+public class ejercicio10_formula
+{
+	public static void main(String[] arg)
+	{
+		double valor=10*5-(5*2)/3;/*Declaro una variable double ingreso el valor de la misma mediante una operacion*/
+		System.out.println(valor);/*Pongo el objeto System.out y llamo al metodo println con la variable valor*/
+	}
+}  
