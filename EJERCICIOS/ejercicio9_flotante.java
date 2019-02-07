0063357ebf2768c55593b19ff614a09dcb099467
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-07 12:46:45 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio9_flotante.java
@@ -0,0 +1,14 @@
+/*Nombre:Ulises Hernandez */
+/*Dia: 07/02/2019 */
+/*Hora: 12:44*/
+
+
+
+public class ejercicio9_flotante 
+{
+	public static void main(String[] arg)
+	{
+		System.out.println((30*5.5-10.3)/(25*3.5-9.5));/*Pongo el objeto System.out y llamo al metodo println para mostrar el mensaje en pantalla*/
+		
+	}
+}   
