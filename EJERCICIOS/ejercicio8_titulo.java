71b80cecfe46a66954449d42f664bec7e9712fbd
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-07 12:43:16 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio8_titulo.java
@@ -0,0 +1,16 @@
+/*Nombre:Ulises Hernandez */
+/*Dia: 07/02/2019 */
+/*Hora: 12:31*/
+
+
+
+public class ejercicio8_titulo 
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("   J    A  V     V   A");/*Pongo el objeto System.out y llamo al metodo println para mostrar el mensaje en pantalla*/
+		System.out.println("   J   A A  V   V   A A");
+		System.out.println("J  J  AAAAA  V V   AAAAA");
+		System.out.println(" JJ  A     A  V   A     A");
+	}
+}   
