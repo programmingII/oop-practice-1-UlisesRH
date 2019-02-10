bfc04e408add4234b16699174b8ed0f9ad4e6a2b
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-10 13:59:53 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio14_bandera.java
@@ -0,0 +1,27 @@
+/*Nombre:Ulises Hernandez */
+/*Dia: 10/02/2019 */
+/*Hora: 1:53*/
+
+
+
+public class ejercicio14_bandera
+{
+	public static void main(String[] args)
+	{
+		System.out.println("* * * * * *================================");/*Ago uso del objeto system.out que es lo que representa una salida estandar*/
+		System.out.println("* * * * *==================================");
+		System.out.println("* * * * * *================================");
+		System.out.println("* * * * *==================================");
+		System.out.println("* * * * * *================================");
+		System.out.println("* * * * *==================================");
+		System.out.println("* * * * * *================================");
+		System.out.println("* * * * *==================================");
+		System.out.println("* * * * * *================================");
+		System.out.println("===========================================");
+		System.out.println("===========================================");
+		System.out.println("===========================================");
+		System.out.println("===========================================");
+		System.out.println("===========================================");
+		System.out.println("===========================================");
+	}
+}
