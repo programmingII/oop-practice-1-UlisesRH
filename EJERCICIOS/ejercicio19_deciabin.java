16aaa2c58a4ebef1369e20b0c625e87a6eb8d62a
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-11 09:14:32 -0700

    Update ejercicio19_deciabin.java

--- EJERCICIOS/ejercicio19_deciabin.java
+++ EJERCICIOS/ejercicio19_deciabin.java
@@ -11,11 +11,11 @@ public class ejercicio19_deciabin
 	{
 		Scanner entrada=new Scanner(System.in);/*Creo un objeto de la clase scanner y como el dispositivo de etrada es el teclado pongo el system.in*/
 		
-		int numero1;
+		int numero1;/*Defino una variable entera*/
 
 		System.out.println("Ingrese el nmumero en decimal");
 		numero1=entrada.nextInt();
 
-		System.out.println(Integer.toBinaryString(numero1));
+		System.out.println(Integer.toBinaryString(numero1));/*utilizo el metodo tobinarystring de la clase intger , ya ueme devuelveuna represemtacion en formato string del argumento tomado, y lo retorna como entero sin signo y con base 2*/
 	}
 }
