9b6d9e52858ca4529b107902d63c03848e188870
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-10 20:20:51 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio16_robot.java
@@ -0,0 +1,18 @@
+/*Nombre:Ulises Hernandez */
+/*Dia: 10/02/2019 */
+/*Hora: 8:14*/
+
+
+
+public class ejercicio16_robot
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println(" +\"\"\"\"\"+ ");/*"En esta parte no podemos poner las comillas sin el slash, puesto que java detecta que estas cerrando el espacio de texto que el sistema muestra en pantalla*/ 
+		System.out.println("[| o o |]"); 
+		System.out.println(" |  ^  |"); 
+		System.out.println(" | '-' |"); 
+		System.out.println(" +-----+"); 	
+	}
+}
