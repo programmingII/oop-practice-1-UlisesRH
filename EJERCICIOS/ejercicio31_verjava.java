9b30cb5cc11bc59274f2ea5d244c556adcae0640
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-17 18:34:49 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio31_verjava.java
@@ -0,0 +1,17 @@
+/*Nombre:Ulises Hernandez*/
+/*Fecha 17/02/2013*/
+/*Hora 6:26 pm*/
+
+public class ejercicio31_verjava
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Version: "+System.getProperties());/*llamo al metodo getproperti de la clase system, esta nos da absolutamente todas las especificaciones del java que este instalado en la computadora*/
+    
+	}	
+
+}
+
+
+
+
