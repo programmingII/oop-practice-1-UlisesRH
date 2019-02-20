eada636daebdf7c2fd027a0d8b208961408b71e4
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-19 22:25:24 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio41_valorasci.java
@@ -0,0 +1,16 @@
+/*Nombre:Ulises Hernandez*/
+/*Fecha 1/02/2019*/
+/*Hora 10:17 pm*/
+
+public class ejercicio41_valorasci
+{
+	public static void main(String[] args)
+	{	
+		char valor='Z';/*declaro una variable de tipo char a la que le asigno la Z*/
+		int valorAsci=(int)valor;/*conierto explicitamente un tipo de dato char a entero*/
+		
+		System.out.println("El ascii de "+valor+" es: "+valorAsci);/*utilizo el metodo println de la clase out para mostrar en pantalla el resultado*/
+		
+	}	
+
+}
