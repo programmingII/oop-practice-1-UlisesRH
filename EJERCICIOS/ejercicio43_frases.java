4ad27d5a1cd7b29a50fc7e6830020d4c585102b0
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-19 22:44:32 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio43_frases.java
@@ -0,0 +1,13 @@
+/*Nombre: Ulises Hernandez*/
+/*Fecha:19/02/19*/
+/*Hora: 10:41 pm*/
+
+
+public class ejercicio43_frases
+{
+	public static void main(String[] args)
+	{	
+		System.out.println("\nTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");
+	}
+}
+/*en este programa lo interesante es el \n y el \t, ambas son secuencias de escape,es decir constantes de utilidad, la n no proporciona un salto de linea, y la t una tabulacion en el texto*/
