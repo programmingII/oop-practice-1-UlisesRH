b5c48f5ef380e38d73ea2b2d950d8bbb361bd438
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-21 12:57:24 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio46_date.java
@@ -0,0 +1,14 @@
+/*Nombre: Ulises Hernandez*/
+/*Fecha:21/02/19*/
+/*Hora: 12:45 pm*/
+
+
+import java.util.Date;/*importo la clase Date el cual maneja datos de tiempo, esta se deriva de util*/
+public class ejercicio46_date
+{
+	public static void main(String[] args)
+	{
+		Date date=new Date();/*creo un objeto de tipo date para usar sus metodos*/
+		System.out.println(date.toString());
+	}
+}
