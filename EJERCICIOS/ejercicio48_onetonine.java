b8d314364638197044ae56645c0f25140be7c8d8
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-21 13:14:57 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio48_onetonine.java
@@ -0,0 +1,15 @@
+/*Nombre: Ulises Hernandez*/
+/*Fecha:21/02/19*/
+/*Hora: 01:10 pm*/
+
+
+public class ejercicio48_onetonine
+{
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=99;i++)/*utilizp estructura de control for para llegar hasta el 99*/
+		{
+			System.out.println(i);
+		}
+	}
+}
