fd1377138657da8190aa6a838f1bfc6adbda76ec
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-23 17:33:53 -0700

    Add files via upload

--- EJERCICIOS/ejercicio62_veinteomenos.java
+++ EJERCICIOS/ejercicio62_veinteomenos.java
@@ -1,6 +1,6 @@
 /*Nombre: Ulises Hernandez*/
 /*Fecha:23/02/19*/
-/*Hora: 05:05pm*/
+/*Hora: 05:10 pm*/
 
 import java.util.Scanner;/*importo la clase scanner el cual me permite manejar los datos, recibiendolos en el sistema, esto viene del paquete util de java*/
 public class ejercicio62_veinteomenos
@@ -15,13 +15,25 @@ public class ejercicio62_veinteomenos
 		System.out.print("Ingresa el numero ");/*utilizo el metodo print de la clase out, el cual me permite imprimir algo en el pantalla*/
 	 	int numero2=entrada.nextInt();/*utilizo el objeto creado de tipo scanner para usar el metodo de ingreso de datos, y guardo ese dato en la variable numero1*/
 		
-		System.out.print("Ingresa el numero ");/*utilizo el metodo print de la clase out, el cual me permite imprimir algo en el pantalla*/
-	 	int numero3=entrada.nextInt();/*utilizo el objeto creado de tipo scanner para usar el metodo de ingreso de datos, y guardo ese dato en la variable numero1*/
-			
-		
-		if((Math.abs(numero1-numero2)>=20 || Math.abs(numero2-numero3)>=20 || Math.abs(numero3-numero2)>=20))/*utilizo una estructura de seleccion simple if para verificar si las restas dan mas de 20, utilizo el metodo abs, de la clase math el cual me da el valor absoluto de las operaciones*/
-			System.out.println("True");
+		System.out.println(resultado(numero1,numero2));
+	}
 
-		System.out.println("False");
+	public static int resultado(int a,int b)
+	{
+		if(a==b)
+			return 0;
+		
+		if(a%6==b%6)
+			if(a>b)
+				return b;
+			else
+				return a;	
+
+		if(a>b)
+			return a;
+		else
+			return b;
+			
+	
 	}
 }
