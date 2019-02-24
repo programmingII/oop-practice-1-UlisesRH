f554efa47e83d797b4cce9cb8b29ac3b60708bf2
159965 UlisesHernandez <44282854+159965-UlisesHernandez@users.noreply.github.com>
2019-02-23 17:39:27 -0700

    Add files via upload

---
+++ EJERCICIOS/ejercicio63_masgrande.java
@@ -0,0 +1,39 @@
+/*Nombre: Ulises Hernandez*/
+/*Fecha:23/02/19*/
+/*Hora: 05:10 pm*/
+
+import java.util.Scanner;/*importo la clase scanner el cual me permite manejar los datos, recibiendolos en el sistema, esto viene del paquete util de java*/
+public class ejercicio63_masgrande
+{
+	public static void main(String[] args)
+	{
+		Scanner entrada=new Scanner(System.in);/*creo un objeto del tipo scanner de la clase utl para utilizar sus metodos e invoco al constructor dandole como parametro la entrada del sistema*/
+		
+     		System.out.print("Ingresa el numero ");/*utilizo el metodo print de la clase out, el cual me permite imprimir algo en el pantalla*/
+	 	int numero1=entrada.nextInt();/*utilizo el objeto creado de tipo scanner para usar el metodo de ingreso de datos, y guardo ese dato en la variable numero1*/
+		
+		System.out.print("Ingresa el numero ");/*utilizo el metodo print de la clase out, el cual me permite imprimir algo en el pantalla*/
+	 	int numero2=entrada.nextInt();/*utilizo el objeto creado de tipo scanner para usar el metodo de ingreso de datos, y guardo ese dato en la variable numero1*/
+		
+		System.out.println(resultado(numero1,numero2));
+	}
+
+	public static int resultado(int a,int b)
+	{
+		if(a==b)
+			return 0;
+		
+		if(a%6==b%6)
+			if(a>b)
+				return b;
+			else
+				return a;	
+
+		if(a>b)
+			return a;
+		else
+			return b;
+			
+	
+	}
+}
